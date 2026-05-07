db42345c4c432d3b77cbeb190cbe2913a25a314c
Michael Rocha <michaelrocha9722@gmail.com>
2026-05-07 12:05:23 -0300

    fix lucide install, color adjustment e create Icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { Trash } from "lucide-react";
+import Icon from "./ui/Icon";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="p-5 flex flex-col gap-4 bg-yellow-500">
+      <Trash className="fill-transparent size-10" />
+      <Icon
+        svg={Trash}
+        color="primary"
+        size="sm"
+        animation="none"
+      />
+      <Icon
+        svg={Trash}
+        color="secondary"
+        size="md"
+        animation="none"
+      />
+      <Icon
+        svg={Trash}
+        color="tertiary"
+        size="lg"
+        animation="spin"
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ui/Icon.tsx
@@ -0,0 +1,54 @@
+import type { ComponentProps, FC } from "react";
+import { type LucideProps } from "lucide-react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const iconVariants = tv({
+  base: "",
+  variants: {
+    color: {
+      primary: "text-black",
+      secondary: "text-red-300",
+      tertiary: "text-white",
+    },
+    size: {
+      sm: "size-4",
+      md: "size-5",
+      lg: "size-10",
+    },
+    animation: {
+      none: "",
+      spin: "animate-spin",
+    },
+  },
+  defaultVariants: {
+    color: "primary",
+    size: "sm",
+    animation: "none",
+  },
+});
+
+type IconProps = Omit<
+  ComponentProps<"svg">,
+  keyof VariantProps<typeof iconVariants>
+>
+  & VariantProps<typeof iconVariants> & {
+    svg: FC<LucideProps>;
+  };
+
+function Icon({
+  svg: IconComponent,
+  color,
+  size,
+  animation,
+  className,
+  ...props
+}: IconProps) {
+  return (
+    <IconComponent
+      className={iconVariants({ color, size, animation, className })}
+      {...props}
+    />
+  );
+}
+
+export default Icon;
