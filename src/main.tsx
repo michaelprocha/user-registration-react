e8d2f1893e8c869e77ca1ff3737df03eac71a1d6
Michael Rocha <michaelrocha9722@gmail.com>
2026-05-07 10:54:48 -0300

    add font, colors and install router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// Supports weights 100-900
+import "@fontsource-variable/inter/wght.css";
+// Supports weights 300-700
+import "@fontsource-variable/space-grotesk/wght.css";
 import "./styles/style.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
