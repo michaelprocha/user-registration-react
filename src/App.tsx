abe07898491f953b5055c0275c08d6661fa5472b
Michael Rocha <michaelrocha9722@gmail.com>
2026-05-08 14:13:04 -0300

    create component Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import { Trash } from "lucide-react";
 import Icon from "./ui/Icon";
+import Skeleton from "./ui/Skeleton";
 
 function App() {
   return (
-    <div className="p-5 flex flex-col gap-4 bg-yellow-500">
-      <Trash className="fill-transparent size-10" />
-      <Icon
-        svg={Trash}
-        color="primary"
-        size="sm"
-        animation="none"
-      />
-      <Icon
-        svg={Trash}
-        color="secondary"
-        size="md"
-        animation="none"
-      />
-      <Icon
-        svg={Trash}
-        color="tertiary"
-        size="lg"
-        animation="spin"
-      />
-    </div>
+    <>
+      <div className="p-5 flex flex-col gap-4 bg-yellow-500">
+        <Trash className="fill-transparent size-10" />
+        <Icon
+          svg={Trash}
+          color="primary"
+          size="sm"
+          animation="none"
+        />
+        <Icon
+          svg={Trash}
+          color="secondary"
+          size="md"
+          animation="none"
+        />
+        <Icon
+          svg={Trash}
+          color="tertiary"
+          size="lg"
+          animation="spin"
+        />
+      </div>
+      <div className="p-5 flex flex-col gap-4">
+        <Skeleton className="h-21.5 border-2 md:border-y-2 md:border-x-0 border-gray-500" />
+        <Skeleton className="h-21.5 border-2 md:border-y-2 md:border-x-0 border-gray-100" />
+      </div>
+    </>
   );
 }
 
